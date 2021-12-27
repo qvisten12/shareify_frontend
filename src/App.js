583969155c826c4a7700fdcc9300a7d0b2bbdd33
Felix Ljungqvist <75997643+qvisten12@users.noreply.github.com>
2021-12-27 13:12:34 +0100

    Implemented posts and categories

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route, useNavigate } from "react-router-dom";
-import Login from "./components/Login";
+import { Login } from "./components";
 import Home from "./container/Home";
 
 function App() {
