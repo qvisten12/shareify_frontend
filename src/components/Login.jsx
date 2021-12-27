ec9d030b7f56cae2dab9024eef4bea84d7dc2d6e
Felix Ljungqvist <75997643+qvisten12@users.noreply.github.com>
2021-12-27 18:08:58 +0100

    login fix

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,20 +9,15 @@ import { client } from "../client";
 
 const Login = () => {
   const navigate = useNavigate();
-
   const responseGoogle = (response) => {
     localStorage.setItem("user", JSON.stringify(response.profileObj));
-
     const { name, googleId, imageUrl } = response.profileObj;
-
-    // creating a new user document
     const doc = {
       _id: googleId,
       _type: "user",
       userName: name,
       image: imageUrl,
     };
-
     client.createIfNotExists(doc).then(() => {
       navigate("/", { replace: true });
     });
@@ -30,7 +25,7 @@ const Login = () => {
 
   return (
     <div className="flex justify-start items-center flex-col h-screen">
-      <div className="relative w-full h-full">
+      <div className=" relative w-full h-full">
         <video
           src={shareVideo}
           type="video/mp4"
@@ -41,26 +36,22 @@ const Login = () => {
           className="w-full h-full object-cover"
         />
 
-        <div
-          className="absolute flex flex-col justify-center items-center top-0 right-0
-        left-0 bottom-0 bg-blackOverlay"
-        >
-          <div className="p-5 bg-mainColor rounded-lg outline-none">
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
+          <div className="p-5 bg-mainColor rounded-lg cursor-pointer outline-none mb-5">
             <img src={logo} alt="logo" />
           </div>
 
-          <div className="shadow-2xl ">
+          <div className="shadow-2xl">
             <GoogleLogin
-              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
               render={(renderProps) => (
                 <button
                   type="button"
-                  className="bg-mainColor mt-5
-                flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
                   onClick={renderProps.onClick}
                   disabled={renderProps.disabled}
                 >
-                  <FcGoogle className="mr-4" /> Sign in with Google
+                  <FcGoogle className="mr-4" /> Sign in with google
                 </button>
               )}
               onSuccess={responseGoogle}
